f740a072be02b3254d4354d281b53e1255608fe7
goncy <gonzalo.pozzo4@gmail.com>
2020-10-08 18:03:43 -0300

    Update config

--- generators/react/eslint/typescript/index.js
+++ generators/react/eslint/typescript/index.js
@@ -6,16 +6,11 @@ module.exports = class extends Generator {
       [
         "@typescript-eslint/eslint-plugin",
         "@typescript-eslint/parser",
-        "babel-eslint",
         "eslint",
         "eslint-config-prettier",
-        "eslint-config-react-app",
-        "eslint-plugin-cypress",
-        "eslint-plugin-flowtype",
-        "eslint-plugin-import",
-        "eslint-plugin-jsx-a11y",
         "eslint-plugin-prettier",
         "eslint-plugin-react",
+        "eslint-plugin-import",
         "eslint-plugin-react-hooks",
         "prettier",
       ],
@@ -24,6 +19,6 @@ module.exports = class extends Generator {
   }
 
   eslint() {
-    this.fs.copy(this.templatePath(".eslintrc.js"), this.destinationPath(".eslintrc.js"));
+    this.fs.copy(this.templatePath(".eslintrc"), this.destinationPath(".eslintrc"));
   }
 };
