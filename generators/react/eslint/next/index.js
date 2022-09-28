dc84237d5244fd3cddc95db3073afabcd5706cbb
goncy <gonzalo.pozzo4@gmail.com>
2022-09-28 11:55:45 -0300

    Up to date

--- generators/react/eslint/next/index.js
+++ generators/react/eslint/next/index.js
@@ -2,27 +2,27 @@ const Generator = require("yeoman-generator");
 
 module.exports = class extends Generator {
   installDependencies() {
-    this.npmInstall(
-      [
-        "prettier",
-        "eslint",
-        "eslint-config-prettier",
-        "eslint-config-standard",
-        "eslint-config-next",
-        "eslint-plugin-import",
-        "eslint-plugin-node",
-        "eslint-plugin-prettier",
-        "eslint-plugin-promise",
-        "eslint-plugin-react",
-        "eslint-plugin-react-hooks",
-        "@typescript-eslint/parser",
-        "@typescript-eslint/eslint-plugin",
-      ],
-      {"save-dev": true}
-    );
+    this.spawnCommandSync("pnpm", [
+      "add",
+      "-D",
+      "prettier",
+      "eslint",
+      "eslint-config-prettier",
+      "eslint-config-standard",
+      "eslint-config-next",
+      "eslint-plugin-import",
+      "eslint-plugin-node",
+      "eslint-plugin-n",
+      "eslint-plugin-prettier",
+      "eslint-plugin-promise",
+      "eslint-plugin-react",
+      "eslint-plugin-react-hooks",
+      "@typescript-eslint/parser",
+      "@typescript-eslint/eslint-plugin",
+    ]);
   }
 
   eslint() {
-    this.fs.copy(this.templatePath(".eslintrc"), this.destinationPath(".eslintrc"));
+    this.fs.copy(this.templatePath(".eslintrc.json"), this.destinationPath(".eslintrc.json"));
   }
 };
